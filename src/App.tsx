a6ca712c05505d086bf71a6a3424999ae5c9eed3
Fikri Karim <fkfikrikarim@gmail.com>
2020-07-07 16:58:40 +0700

    Add @monaco-editor/react for the editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  ControlledEditor,
+  ControlledEditorOnChange,
+} from "@monaco-editor/react";
+
+import "./App.css";
 
 function App() {
+  const handleEditorChange: ControlledEditorOnChange = (event, value) => {
+    console.log(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ControlledEditor
+        height="90vh"
+        value={"# Markdown preview"}
+        onChange={handleEditorChange}
+        language="markdown"
+      />
     </div>
   );
 }
