1e194798ebfc891f32d15b8152d5a4cc8592ed2d
Fikri Karim <fkfikrikarim@gmail.com>
2020-07-07 17:34:47 +0700

    Add toggleTheme button on the TopRight

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,7 @@ import ReactMarkdown from "react-markdown";
 
 import "./App.css";
 
-function App() {
-  const defaultValue = `# Markdown Preview
+const defaultEditorValue = `# Markdown Preview
 ## This is header 2
 
 _ Italicia 🇮🇹 _
@@ -16,16 +15,36 @@ _ Italicia 🇮🇹 _
 ** boldie **
 `;
 
-  const [editorValue, setEditorValue] = useState(defaultValue);
+enum Theme {
+  "light" = "light",
+  "dark" = "dark",
+}
+
+function App() {
+  const [editorValue, setEditorValue] = useState(defaultEditorValue);
+  const [theme, setTheme] = useState<Theme>(Theme.light);
 
   const handleEditorChange: ControlledEditorOnChange = (event, value) => {
     setEditorValue(String(value));
   };
 
+  const toggleTheme = () => {
+    if (theme === Theme.light) {
+      setTheme(Theme.dark);
+    } else {
+      setTheme(Theme.light);
+    }
+  };
+
   return (
     <div className="App">
+      <button onClick={toggleTheme} className="toggleButton">
+        Toggle theme
+      </button>
+
       <div className="column">
         <ControlledEditor
+          theme={theme}
           height="100vh"
           value={editorValue}
           onChange={handleEditorChange}
